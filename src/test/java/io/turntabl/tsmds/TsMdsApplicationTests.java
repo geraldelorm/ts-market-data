b3a17f4108d838bdddda495aad2089012178116b
geraldelorm <geraldelorm14@gmail.com>
2021-11-26 12:48:54 +0000

    auto-test testing

--- src/test/java/io/turntabl/tsmds/TsMdsApplicationTests.java
+++ src/test/java/io/turntabl/tsmds/TsMdsApplicationTests.java
@@ -1,5 +1,6 @@
 package io.turntabl.tsmds;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class TsMdsApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assertions.assertEquals(1,2);
 	}
 
 }
