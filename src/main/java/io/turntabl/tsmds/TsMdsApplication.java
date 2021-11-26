c39049de2bdc923b3f787675190f86d6c881a2df
geraldelorm <geraldelorm14@gmail.com>
2021-11-26 12:26:44 +0000

    initial commit

---
+++ src/main/java/io/turntabl/tsmds/TsMdsApplication.java
@@ -0,0 +1,13 @@
+package io.turntabl.tsmds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsMdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TsMdsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/turntabl/tsmds/TsMdsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.turntabl.tsmds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TsMdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
