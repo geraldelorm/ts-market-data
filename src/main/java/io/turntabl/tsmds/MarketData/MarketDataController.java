18253839bb4ef6e0bb00d0003e35da7de9b76daa
geraldelorm <geraldelorm14@gmail.com>
2021-11-28 14:13:32 +0000

    marketdata subscription for exchange1

--- src/main/java/io/turntabl/tsmds/MarketData/MarketDataController.java
+++ src/main/java/io/turntabl/tsmds/MarketData/MarketDataController.java
@@ -1,31 +1,30 @@
-package io.turntabl.tsmds.Stock;
+package io.turntabl.tsmds.MarketData;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
 @RestController
-public class StockController {
+public class MarketDataController {
 
     @Autowired
-    StockService stockService;
+    MarketDataService marketDataService;
 
     @GetMapping
-    public String home(){
+    public String home() {
         return "Hello, Welcome to Market Data Service";
     }
 
     @GetMapping("/stocks")
-    public List<Stock> getStocksData(){
-        return stockService.getAllStocks();
+    public List<Stock> getStocksData() {
+        return marketDataService.getMarketData();
     }
 
     @GetMapping("/stocks/{ticker}")
-    public Stock getStockDataForTicker(@PathVariable String ticker) {
-        return stockService.getStock(ticker);
+    public String getStockDataForTicker(@PathVariable String ticker) {
+        return marketDataService.getStockData(ticker);
     }
 }
