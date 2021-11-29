5a56d40158ce5c05fcc9a8fecf883fc9aa54cc7c
geraldelorm <geraldelorm14@gmail.com>
2021-11-29 18:23:24 +0000

    redis messaging working locally

--- src/main/java/io/turntabl/tsmds/MarketData/Webhook.java
+++ src/main/java/io/turntabl/tsmds/MarketData/Webhook.java
@@ -1,6 +1,8 @@
 package io.turntabl.tsmds.MarketData;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,15 +16,27 @@ public class Webhook {
     @Autowired
     MarketDataService marketDataService;
 
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Autowired
+    private ChannelTopic channelTopic;
+
     @PostMapping("/md")
     public String setMarketDataFromExchange(@RequestBody List<MarketData> marketDataFromExchange) {
+        //send data to listener
+        redisTemplate.convertAndSend(channelTopic.getTopic(), marketDataFromExchange);
+
         marketDataService.updateMarketData(marketDataFromExchange);
-        return "data submitted";
+        return "data submitted and data should be published";
     }
 
     @PostMapping("/md2")
     public String setMarketDataFromExchange2(@RequestBody List<MarketData> marketDataFromExchange) {
+        //send data to listener
+        redisTemplate.convertAndSend(channelTopic.getTopic(), marketDataFromExchange);
+
         marketDataService.updateMarketData2(marketDataFromExchange);
-        return "data submitted";
+        return "data submitted and data should be published";
     }
 }
