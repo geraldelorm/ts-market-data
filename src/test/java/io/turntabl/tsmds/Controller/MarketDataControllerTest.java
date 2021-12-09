13211e34cfda0cb7f2dc8f9080280fb9ca373623
benneth-yankey <benneth.yankey@turntabl.io>
2021-12-09 16:17:29 +0000

    added tests for market controller

--- src/test/java/io/turntabl/tsmds/Controller/MarketDataControllerTest.java
+++ src/test/java/io/turntabl/tsmds/Controller/MarketDataControllerTest.java
@@ -1,20 +1,70 @@
 package io.turntabl.tsmds.Controller;
 
+import io.turntabl.tsmds.Services.MarketDataService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.MockitoAnnotations.openMocks;
 
 class MarketDataControllerTest {
+    private AutoCloseable closeable;
 
-    @Test
-    void sendInitialData() {
+    @InjectMocks
+    private MarketDataController marketDataController;
+
+    @Mock
+    private MarketDataService marketDataService;
+
+    @Mock
+    private RestTemplate restTemplate;
+
+    @BeforeEach
+    public void openMock() {
+        closeable = openMocks(this);
+    }
+
+    @AfterEach
+    public void closeMock() throws Exception {
+        closeable.close();
     }
 
     @Test
-    void setMarketDataFromExchange() {
+    void itShouldCallRestTemplate2Times_AfterConstructorIsInitialized() {
+       marketDataController.sendInitialData();
+
+       verify(restTemplate, times(2)).getForObject(anyString(), any());
     }
 
     @Test
-    void setMarketDataFromExchange2() {
+    void itShouldReturnMessage_WhenReceivedDataFromExchangeOne() {
+        String expected = "Data from exchange1 sent";
+
+        assertEquals(expected, marketDataController.setMarketDataFromExchange(List.of()));
+    }
+
+    @Test
+    void itShouldCallMarketDataServiceOnce_WhenItIsInvoked() {
+        marketDataController.setMarketDataFromExchange(List.of());
+        marketDataController.setMarketDataFromExchange2(List.of());
+
+        verify(marketDataService, times(1)).sendMessageFromExchangeOne(List.of());
+        verify(marketDataService, times(1)).sendMessageFromExchangeTwo(List.of());
+    }
+
+    @Test
+    void itShouldReturnMessage_WhenReceivedDataFromExchangeTwo() {
+        String expected = "Data from exchange2 sent";
+
+        assertEquals(expected, marketDataController.setMarketDataFromExchange2(List.of()));
     }
 }
